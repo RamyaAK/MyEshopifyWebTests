6d7dc6a061699387ea8ed653c375cca6c63af176
Ramya Kulkarni <ramyak@testvagrant.com>
2020-04-30 07:04:38 +0530

    Implementation of singleton design patterns for Pages - Added HomePage, CatalogPage, ProductDetailsPage and CartPage in singleton package. - Refactored LoginPage. - Added MyShopify Tests and added assertion.

--- src/test/java/singletonTests/MyShopifyTests.java
+++ src/test/java/singletonTests/MyShopifyTests.java
@@ -1,12 +1,15 @@
 package singletonTests;
 
+import builders.ProductBuilder;
 import com.myshopify.automate.tests.BaseTest;
+import entities.Product;
+import entities.SearchByDropDown;
 import org.testng.annotations.Test;
-import singletonPages.LoginPage;
+import singletonPages.*;
 
 public class MyShopifyTests extends BaseTest {
 
-    @Test
+    @Test(alwaysRun = true,priority = 1)
     public void shouldBeAbleToLogin(){
 
         LoginPage loginPage = LoginPage.getInstance();
@@ -15,7 +18,58 @@ public class MyShopifyTests extends BaseTest {
                 .navigateToLoginPage(baseUrl)
                 .login(password)
                 .assertLoginSuccessful("ecom.optimus");
+    }
 
+    @Test(alwaysRun = true, priority = 2)
+    public void shouldSearchForProductAndAddToCart() throws InterruptedException {
+
+        Product product = new ProductBuilder().build();
+
+        LoginPage loginPage = LoginPage.getInstance();
+        HomePage homePage = HomePage.getInstance();
+        SearchPage searchPage = SearchPage.getInstance();
+        ProductDetailsPage productDetailsPage= ProductDetailsPage.getInstance();
+
+        loginPage
+                .navigateToLoginPage(baseUrl)
+                .login(password);
+        homePage
+                .navigateToSearchPage();
+
+        searchPage
+                .searchForProduct(product)
+                .selectProduct(product);
+
+        productDetailsPage
+                .addProductToCart()
+                .assertThatProductAddedToCart(product);
+    }
+
+    @Test(alwaysRun = true, priority=3)
+    public void shouldAddProductFromFeaturedCollections() throws InterruptedException {
+
+        Product product = new ProductBuilder().build();
+
+        LoginPage loginPage = LoginPage.getInstance();
+        HomePage homePage = HomePage.getInstance();
+        SearchPage searchPage = SearchPage.getInstance();
+        ProductDetailsPage productDetailsPage= ProductDetailsPage.getInstance();
+        CatalogPage catalogPage = CatalogPage.getInstance();
+
+        loginPage
+                .navigateToLoginPage(baseUrl)
+                .login(password);
+        homePage
+                .navigateToCatalogPage();
+        catalogPage
+                .selectFeaturedOption(SearchByDropDown.Featured);
+        searchPage
+                .selectProduct(product);
+        productDetailsPage
+                .addProductToCart()
+                .assertThatProductAddedToCart(product);
+        catalogPage
+                .assertThatAddedProductIsFromFeaturedCollections(SearchByDropDown.Featured);
     }
 
 }
